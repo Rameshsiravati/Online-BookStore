70f62c0edd7f31d44228545357a539ec8c45e7f3
Rameshsiravati <rameshsiravati2004@gmail.com>
2026-01-03 22:42:12 +0530

    Refactor API endpoints in authService to use relative paths

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,10 +2,10 @@ import api from './api';
 
 export const authService = {
   login: (username, password) =>
-    api.post("http://localhost:8080/api/auth/login", { username, password }),
+    api.post("/auth/login", { username, password }),
 
   register: (username, email, password) =>
-    api.post("http://localhost:8080/api/auth/register", { username, email, password }),
+    api.post("/auth/register", { username, email, password }),
 
   logout() {
     localStorage.removeItem('token');
